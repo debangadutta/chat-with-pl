6176e47ec779854814bc8eceff9f253fdd598746
Debanga <debangadutta2@gmail.com>
2025-06-06 16:09:02 +0200

    updated app.py to deply on streamlit

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from retrieval import (
 )
 
 # Load environment variables
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+api_key = st.secrets["OPENAI_API_KEY"]
 client = OpenAI(api_key=api_key)
 
 # Load CSV
